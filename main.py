a8882f051a483c61c395793bd319c3c38c4465d6
Koo8 <kooeight@gmail.com>
2023-11-20 21:45:31 -0600

    updated decoder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 How to make custom class Json serializable - 4 methods
+How to make json.loads() to return a custom class objeact instead of string type - using object hooks
 """
 import json
 from json import JSONEncoder
@@ -19,12 +20,15 @@ mom = Person("nancy",'n@email.com', [son, daughter] )
 """ Method 1: use subclass JSONEncoder to override default() method """
 class PersonEnCoder(JSONEncoder):
     def default(self, o: Any) -> Any:
-        return o.__dict__
+        """ if json.loads() return dict is ok, use this simple return"""
+        # return o.__dict__
+        """ if json.loads() return the object(person), use this return with a added key of the class name for conditional checking """
+        return {"name":o.name, 'email':o.email, 'children': o.children, o.__class__.__name__: True}
 
 
 # print(PersonEnCoder().encode(mom)) # to test our if encoder works
 momJson = json.dumps(mom, indent=4, cls=PersonEnCoder)
-# print(momJson)
+print(momJson)
 
 mom = json.loads(momJson)
 # print(mom)
@@ -66,6 +70,7 @@ person_pickled = jsonpickle.encode(mom,  indent=4)
 class Person_2(dict):
     def __init__(self, name, email, children=None):
         dict.__init__(self, name=name, email=email,children=children)
+        
 
 grandson = Person_2('Tom', 't@email.com')
 son = Person_2("John", 'j@email.com')
@@ -73,5 +78,23 @@ daughter = Person_2("Lucy", 'l@email.com', grandson)
 mom2 = Person_2("nancy",'n@email.com', [son, daughter] )
 
 mom2_json = json.dumps(mom2, indent=4)
-print(mom2_json)
-print(json.loads(mom2_json))
+# print(mom2_json)
+# print(json.loads(mom2_json)) 
+
+""" custom deCoder for 'object_hook' param for json.loads() to return an object(person) """
+
+def person_decoder(dic_tionary):
+    if Person.__name__ in dic_tionary:
+        return Person(name=dic_tionary['name'], email=dic_tionary['email'], children=dic_tionary['children'])
+    else:
+        return dic_tionary
+    
+# p = json.loads(momJson, object_hook=person_decoder)
+# print(p.children[0].name)
+  
+def person_decoder_without_class_name(dic):
+    return Person(name=dic['name'], email=dic['email'], children=dic['children'])
+
+p1 = json.loads(momJson, object_hook=person_decoder_without_class_name)
+print(p1.name)
+
